1e9b19b68cb45eb984c3326cc9a79036cb6ed770
Stanislav Gurin <sgurin@justcoded.com>
2024-05-03 13:50:44 +0300

    wip: Re-write script to data attr because conflicts with docsify menu plugin

--- html/assets/js/plugins/menu-collapse.js
+++ html/assets/js/plugins/menu-collapse.js
@@ -2,6 +2,7 @@
   const menuCollapse = function (hook, vm) {
     // Hook to execute after the page is fully loaded https://github.com/docsifyjs/docsify/blob/develop/docs/write-a-plugin.md
     hook.doneEach(() => {
+      // Select all parent items
       const parentItems = document.querySelectorAll('.sidebar-nav > ul > li');
 
       parentItems.forEach(item => {
@@ -9,53 +10,43 @@
          * Skip items that already have link or not rendered
          * Check on empty string required because an original docsify menu does not provide hooks after load
          */
-        if (item.firstChild.nodeType !== Node.TEXT_NODE || item.firstChild.textContent.trim() === "") return;
+        const textNode = item.firstChild;
+        if (textNode.nodeType !== Node.TEXT_NODE || textNode.textContent.trim() === "") return;
+
+        // Create link for text item
+        const link = document.createElement('a');
+        link.href = '#';
+        link.textContent = textNode.textContent;
+        textNode.parentNode.replaceChild(link, textNode);
 
         // Check if the parent item has a sublist
         const sublist = item.querySelector('ul');
+        if (!sublist) return;
 
-        if (sublist) {
-          // Check if any nested items in the sublist have the 'active' class
-          const nestedItems = sublist.querySelectorAll('li');
-          const hasActiveItems = Array.from(nestedItems).some(nestedItem => nestedItem.classList.contains('active'));
+        // Check if any nested items in the sublist have the 'active' class
+        const hasActiveItems = sublist.querySelector('.active');
 
-          /**
-           * Add the 'collapse' class to items without active nested items, otherwise add 'active'
-           * That is need to highlight menu item on a page load when subitem is active
-           */
-          if (!hasActiveItems) {
-            item.classList.add('collapse');
-          } else {
-            item.classList.add('active');
-            item.classList.remove('collapse');
-          }
-
-          // Add class to sublist
-          sublist.classList.add('app-sub-sidebar');
-
-          // Wrap text item in a link to follow default menu behavior and classes
-          const textNode = item.firstChild;
-          const link = document.createElement('a');
-          link.href = '#/';
-          link.textContent = textNode.textContent;
-          textNode.parentNode.replaceChild(link, textNode);
-
-          // Add click event listener to link to prevent default behavior
-          link.addEventListener('click', (e) => {
-            e.preventDefault();
-            // Close all other collapsed items when a link is clicked
-            parentItems.forEach(otherItem => {
-              if (otherItem !== item) {
-                otherItem.classList.remove('collapse');
-              }
-            });
-          });
+        /**
+         * Set data-submenu-state attribute to items without active nested items, otherwise set it to 'active'
+         * That is need to highlight menu item on a page load when subitem is active
+         */
+        if (hasActiveItems) {
+          item.setAttribute('data-submenu-state', 'opened');
+        } else {
+          item.setAttribute('data-submenu-state', 'collapsed');
         }
+
+        // Add click event listener to link to toggle submenu state
+        link.addEventListener('click', (e) => {
+          e.preventDefault();
+          const submenuState = item.getAttribute('data-submenu-state');
+          item.setAttribute('data-submenu-state', submenuState === 'collapsed' ? 'opened' : 'collapsed');
+        });
       });
     });
   };
 
   // Add the plugin to the Docsify plugin array
   $docsify = $docsify || {};
-  $docsify.plugins = [].concat(menuCollapse, $docsify.plugins || []);
+  $docsify.plugins = [$docsify.plugins || [], menuCollapse];
 })();
